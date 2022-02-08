ca7aec91fb97587237c0ed820815357614d230f0
Marat Kazgozhin <Marat.Kazgozhin@btsdigital.kz>
2022-02-08 14:10:51 +0600

    screenshot key, add symbols, better base layer symbols positioning

--- keymap.c
+++ keymap.c
@@ -14,6 +14,7 @@
 #define KC_PC_PASTE LCTL(KC_V)
 #define ES_LESS_MAC KC_GRAVE
 #define ES_GRTR_MAC LSFT(KC_GRAVE)
+#define SCREENSHOT_MAC LGUI(LSFT(KC_4))
 #define ES_BSLS_MAC ALGR(KC_6)
 #define NO_PIPE_ALT KC_GRAVE
 #define NO_BSLS_ALT KC_EQUAL
@@ -111,7 +112,7 @@ enum tap_dance_codes {
     SFT_N, KC_BSPACE, TD(DANCE_4), /* LEFT THUMB KEYS */ \
     \
     /* RIGHT HALF */ \
-    AG_BSLS, AG_COLN, AG_EQL, NONE_KEY, AG_QUES, AG_ASTR,  EN_TILD, \
+    AG_BSLS, AG_COLN, AG_EQL, AG_ASTR, AG_QUES, EN_TILD, NONE_KEY, \
     EN_QUOT,    RU1,     RU2,     RU3,     RU4,     RU5,     AG_MINS, \
     AG_COMM,    RH1,     RH2,     RH3,     RH4,     RH5,     RH6, \
              RD1,     RD2,     RD3,     RD4,     RD5,     RD6, \
@@ -196,7 +197,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_SYMBOLS] = MY_layout(
     // LEFT HALF
     TG(L_SYMBOLS), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _______,
-    _______, _______, _______, KC_LPRN,     KC_RPRN,     KC_PIPE, _______,
+    _______, EN_HASH, AG_PERC, KC_LPRN,     KC_RPRN,     KC_PIPE, _______,
     _______, EN_LT,   EN_GT,   KC_LCBR,     KC_RCBR,     _______, _______,
     _______, _______, _______, KC_LBRACKET, KC_RBRACKET, _______,
     _______, _______, _______, _______,     _______,
@@ -216,7 +217,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_NUMBERS] = MY_layout(
     // LEFT HALF
     TG(L_NUMBERS), _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, SCREENSHOT_MAC, _______, _______, _______, _______,
     _______, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_PREV_TRACK, KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN, KC_MEDIA_NEXT_TRACK, _______,
     _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______,
